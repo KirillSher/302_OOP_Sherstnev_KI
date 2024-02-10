5baba0749ffe30eb9692c29530fc7cb2401e7a17
KirillSher <SHERSNE@yandex.ru>
2024-02-10 13:28:15 +0300

    Task01

--- Task01/hello.php
+++ Task01/hello.php
@@ -1,6 +1,6 @@
 <?php
-
-echo date('l jS \of F Y h:i:s A');
-echo "\nФИО:Шерстнев Кирилл";
+date_default_timezone_set("Europe/Moscow");
+echo date('Дата: l jS \of F Y h:i:s A');
+echo "\nФамилия и Имя:Шерстнев Кирилл";
 
 ?>
